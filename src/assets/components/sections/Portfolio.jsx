24557db4892acdfe1f8b12b844c649b693e74cf9
Juan Pablo Spiatta <spiattajuanpablodesarrollador@gmail.com>
2022-10-03 18:03:59 -0300

    adding new project

--- src/assets/components/sections/Portfolio.jsx
+++ src/assets/components/sections/Portfolio.jsx
@@ -4,22 +4,23 @@ import style from '../../styles/Portfolio.module.css';
 import Project from '../Project.jsx';
 
 import img from '../../img/weather-app-img-2.png';
+import img2 from '../../img/goScrum-app-img-2.png';
 
 
 const ListOfProjects = [
     {   
         id: 1,
         imgUrl: img,
-        title: 'El clima',
+        title: 'El clima ⛅',
         info: 'Pagina web del clima, en este proyecto trabaje con la API openweathermap para obtener los pronosticos del tiempo. La web funciona por geolocalizacion y de muestra los datos del clima actual de la ciudad en la que te encuentras y el pronostico de la proxima semana. ',
         link: 'https://app-clima-jps.netlify.app/',
         repository: 'https://github.com/Juan-Pablo-Spiatta/Pagina-clima'
     },
     {   
         id: 2,
-        imgUrl: img,
-        title: 'GoScrum',
-        info: 'Pagina de creacion de tareas, estilo Trello',
+        imgUrl: img2,
+        title: 'GoScrum 🗒',
+        info: 'Pagina de creacion de tareas, estilo Trello. En este proyecto trabaje el Front-end y algo del Back-end. Para el Front tabaje con React.js y varias librerias mas como react-router, formik, yup, axios, skeleton, etc. En el back cree un servidor con Node.js y estoy en camino a crear una base de datos. Si queres ver mas detalles pasate por el repo! 😄',
         link: 'https://juan-pablo-spiatta.github.io/GoScrum/',
         repository: 'https://github.com/Juan-Pablo-Spiatta/GoScrum'
     },
